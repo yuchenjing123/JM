b89a37f7da62a0619181b0e4bcd9f199b5d0b091
yuchenjing123 <steve_jing@163.com>
2020-04-21 10:14:23 +0800

    hello world

---
+++ test.c
@@ -0,0 +1,20 @@
+/* ************************************************************************
+ *       Filename:  test.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  21/04/20 10:13:13
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+
+	printf("hello world");
+	return 0;
+}
+
+
